2b8b7dcfc30afc0979aaf006940fbce200bad8b3
itsechi <itsechi@gmail.com>
2022-06-16 19:51:50 +0200

    Update game with event listeners to play one round

--- script.js
+++ script.js
@@ -3,61 +3,51 @@ let computerScore = 0;
 
 // computerPlay that will randomly return either rock, paper or scissors
 function computerPlay() {
-    let computerSelection = Math.floor(Math.random() * 3); 
-    return (computerSelection == 0) ? 'rock' : (computerSelection == 1) ? 'paper' : 'scissors';
+  let computerSelection = Math.floor(Math.random() * 3); 
+  return (computerSelection == 0) ? 'rock' : (computerSelection == 1) ? 'paper' : 'scissors';
 }
 
-// playerPlay that will ask the user for input and store that input
-function playerPlay() {
-    let playerSelection = prompt('Rock, paper or scissors?').toLowerCase();
-    return playerSelection;
-}
+
+
+const scissors = document.querySelector('#scissors');
+scissors.addEventListener('click', () => {
+  playRound('scissors');
+});
+const rock = document.querySelector('#rock');
+rock.addEventListener('click', () => {
+  playRound('rock');
+});
+const paper = document.querySelector('#paper');
+paper.addEventListener('click', () => {
+  playRound('paper');
+});
+
+
 
 // a single round that takes two parameters playerSelection and computerSelection and returns a string declaring a winner
-function playRound(playerSelection, computerSelection) {
-    console.log('You chose: ' + playerSelection + ', ' + 'computer chose: ' + computerSelection + '!');
+function playRound(playerSelection) {
+  const computerSelection = computerPlay();
+  console.log('You chose: ' + playerSelection + ', ' + 'computer chose: ' + computerSelection + '!');
     
-    if (playerSelection == computerSelection) {
-        return 'It\'s a tie!';
-    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
-        playerScore++;
-        return 'You win!';
-    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
-        computerScore++;
-        return 'You lose!';
-    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
-        playerScore++;
-        return 'You win!';
-    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
-        computerScore++;
-        return 'You lose!';
-    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
-        playerScore++;
-        return 'You win!';
-    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
-        computerScore++;
-        return 'You lose!'
-    } else {
-        alert('Type only rock, paper or scissors');
-    }
+  if (playerSelection == computerSelection) {
+    return 'It\'s a tie!';
+  } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+    playerScore++;
+    return 'You win!';
+  } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+    computerScore++;
+    return 'You lose!';
+  } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    playerScore++;
+    return 'You win!';
+  } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+    computerScore++;
+    return 'You lose!';
+  } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+    playerScore++;
+    return 'You win!';
+  } else {
+    computerScore++;
+    return 'You lose!'
+  }
 }
-
-
-
-// function that lets you play 5 rounds, calculate the score and declare a winner
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        const playerSelection = playerPlay();
-        const computerSelection = computerPlay();
-        console.log(playRound(playerSelection, computerSelection));
-        console.log('Your score: ' + playerScore + ' | ' + 'Computer score: ' + computerScore);
-        console.log('~-~-~-~-~-~-~-~');
-    }
-    if (playerScore > computerScore) {
-        console.log('YOU WIN! CONGRATS!');
-    } else {
-        console.log('YOU LOSE! TRY AGAIN!');
-    }
-}
-
-game();
